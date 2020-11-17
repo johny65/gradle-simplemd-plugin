aa048bb08a02c2aeab5940fe1f6a3909e9e7cc2f
Juan Bertinetti <juanbertinetti@gmail.com>
2020-11-17 18:19:29 -0300

    Different name for extension

--- plugin/src/main/java/com/github/johny65/simplemd/GradleSimpleMDPlugin.java
+++ plugin/src/main/java/com/github/johny65/simplemd/GradleSimpleMDPlugin.java
@@ -11,13 +11,14 @@ public class GradleSimpleMDPlugin implements Plugin<Project> {
 
     private static final String TASK_NAME = "md2html";
     
+    private static final String EXTENSION_NAME = "markdownConvertion";
+    
     @Override
     public void apply(Project project) {
-        var extension = project.getExtensions().create(TASK_NAME, PluginExtension.class);
+        var extension = project.getExtensions().create(EXTENSION_NAME, PluginExtension.class);
         project.getTasks().register(TASK_NAME, ConvertMarkdownTask.class, task -> {
             task.getSources().set(extension.getInputFiles(project));
             task.getOutputDir().set(extension.getOutputDir(project));
-            //task.getOutputs().upToDateWhen(t -> false);
         });
     }
     
